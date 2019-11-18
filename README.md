edf8623b2b9a59241926ee32917ab2b6fb767d97
ovdink <fake@mail.com>
2019-11-18 13:05:42 +0300

    build 0.0.2

--- README.md
+++ README.md
@@ -4,5 +4,13 @@
 
 1. Git clone
 2. Drink some coffee
-3. Write a command: npm i
+3. **Write a commands:**
+
+- **_npm i_** - for downloads packages (dependencies in package.json; node_modules will be created)
+- **_npm install gulp_** in your work repository
+- **_gulp_** - to start the local server (when saved files, the page will refresh)
+- **_gulp build_** - create "dist" rep with files for production
+- **_gulp clean_** - deleate "dist"
+- **_gulp make_** - **_clean_** + **_build_**
+
 4. Happy coding!
--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,51 @@
+'use strict';
+
 const gulp = require('gulp'),
   sass = require('gulp-sass'),
   browserSync = require('browser-sync'),
   uglify = require('gulp-uglify'),
   concat = require('gulp-concat'),
-  rename = require('gulp-rename');
+  rename = require('gulp-rename'),
+  del = require('del'),
+  autoprefixer = require('gulp-autoprefixer');
 
 // for img compression use Squoosh or TinyPng
 
-gulp.task('scss', function() {
+gulp.task('clean', async () => {
+  del.sync('dist');
+});
+
+gulp.task('scss', () => {
   return gulp
     .src('app/scss/**/*.scss')
     .pipe(sass({ outputStyle: 'compressed' }))
+    .pipe(autoprefixer())
     .pipe(rename({ suffix: '.min' }))
     .pipe(gulp.dest('app/css'))
     .pipe(browserSync.reload({ stream: true }));
 });
 
-gulp.task('html', function() {
+gulp.task('css', () => {
+  return gulp
+    .src([
+      'node_modules/normalize.css/normalize.css',
+      'node_modules/slick-carousel/slick/slick.css',
+      'node_modules/magnific-popup/dist/magnific-popup.css'
+    ])
+    .pipe(concat('_libs.scss'))
+    .pipe(gulp.dest('app/scss'))
+    .pipe(browserSync.reload({ stream: true }));
+});
+
+gulp.task('html', () => {
   return gulp.src('app/*.html').pipe(browserSync.reload({ stream: true }));
 });
 
-gulp.task('script', function() {
+gulp.task('script', () => {
   return gulp.src('app/js/*.js').pipe(browserSync.reload({ stream: true }));
 });
 
-gulp.task('js', function() {
+gulp.task('js', () => {
   return gulp
     .src([
       'node_modules/slick-carousel/slick/slick.js',
@@ -35,7 +56,7 @@ gulp.task('js', function() {
     .pipe(gulp.dest('app/js'));
 });
 
-gulp.task('browser-sync', function() {
+gulp.task('browser-sync', () => {
   browserSync.init({
     server: {
       baseDir: 'app/'
@@ -43,10 +64,23 @@ gulp.task('browser-sync', function() {
   });
 });
 
-gulp.task('watch', function() {
+gulp.task('build', async () => {
+  gulp.src('app/**/*.html').pipe(gulp.dest('dist'));
+  gulp.src('app/css/**/*.css').pipe(gulp.dest('dist/css'));
+  gulp.src('app/js/*.js').pipe(gulp.dest('dist/js'));
+  gulp.src('app/fonts/**/*.*').pipe(gulp.dest('dist/fonts'));
+  gulp.src('app/img/**/*.*').pipe(gulp.dest('dist/img'));
+});
+
+gulp.task('watch', () => {
   gulp.watch('app/scss/**/*.scss', gulp.parallel('scss'));
   gulp.watch('app/*.html', gulp.parallel('html'));
   gulp.watch('app/js/*.js', gulp.parallel('script'));
 });
 
-gulp.task('default', gulp.parallel('scss', 'js', 'browser-sync', 'watch'));
+gulp.task('make', gulp.series('clean', 'build'));
+
+gulp.task(
+  'default',
+  gulp.parallel('css', 'scss', 'js', 'browser-sync', 'watch')
+);
